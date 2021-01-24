b9e3dd7b25e385995f2c5d293142a8749cec25e2
PotOfCoffee2Go <PotOfCoffee2Go@users.noreply.github.com>
2021-01-24 01:53:09 -0500

    Add insert, find, update db keys stubs

--- src/keydb.js
+++ src/keydb.js
@@ -1,15 +1,45 @@
 const _projectdir = require('path').resolve(__dirname, '..');
 
 const { datastore } = require('nedb-promise');
-var keydb;
+var db;
 
 const openKeyDb = async () => {
-  keydb = datastore({
+  db = datastore({
      filename: _projectdir + '/private/key.db',
      autoload: true // so that we don't have to call loadDatabase()
   })
+  return await db.count({});
+};
 
-  return await keydb.count({});
+const record = {
+  name: '',
+  email: '',
+  public: '',
+  private: ''
+};
+
+
+const parseBlock = (pgpblock) => {
+
 }
 
+const keydb = {
+  findByName: async (name) => {
+    let rec = await db.find({name});
+    return rec;
+  },
+  findByEmail: async (email) => {
+    let rec = await db.find({email});
+    return rec;
+  },
+  insert: async (pgpblock) => {
+
+  },
+  update: async (pgpblock) => {
+
+  },
+}
+
+
 exports.openKeyDb = openKeyDb;
+exports.keydb = keydb;
